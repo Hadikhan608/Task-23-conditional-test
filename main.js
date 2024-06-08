a50ef4e989333d74dc985bbbe3a61c239eba1835
Abdul Hadi <161719851+Hadikhan608@users.noreply.github.com>
2024-06-08 13:37:14 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+"use strict";
+//Conditional Tests: Write a series of conditional tests. Print a statement describing each
+// test and your prediction for the results of each test. Your code should look something like this:
+//let car = ‘subaru’;
+//console.log(“Is car == ‘subaru’? I predict True.”)
+//console.log(car == ‘subaru’)
+//• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+//• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False
+//Test 1
+let myCar = "BMW";
+console.log(myCar == "BMW");
+//Test 2
+let myFruit = "Kiwi";
+console.log(myFruit == "Kiwi");
+//Test 3
+let myArray = ["Kumail", "Taha", "Shahid"];
+console.log(myArray.length == 3);
+//test 4
+let myStudent = "Khadija";
+console.log(myStudent == "Khadija");
+//test 5
+let myTeacher = "Sir Hamzah";
+console.log(myTeacher == "Sir Hamzah");
+//test 6
+let Array1 = [10, 20, 30, 40];
+console.log(Array1.length == 5);
+//test 7
+let num1 = 3;
+var number = 5;
+console.log(num1 + number == 7);
+//test 8
+let num2 = 10;
+let num9 = 20;
+console.log(num2 * num9 == 7);
+//test 9
+let randomNum = 25;
+let randomNum2 = 8;
+console.log(randomNum < randomNum2);
+//test 10
+let value = 45;
+let value2 = 1;
+console.log(value / value2 == 1);
